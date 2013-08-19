d65c1b9c0acc9e2ed43bc946191d5cbb936aad43
Jaakko Koivuniemi <oh7bf@sral.fi>
2013-08-19 21:58:11 +0300

    pipictest.c count clock cycles and read analog inputs.
    Print time stamp for longer testing periods.

--- pipictest/pipictest.c
+++ pipictest/pipictest.c
@@ -20,7 +20,7 @@
 ****************************************************************************
 *
 * Wed Aug 14 22:33:01 CEST 2013
-* Edit: 
+* Edit: Mon Aug 19 13:44:18 EEST 2013
 *
 * Jaakko Koivuniemi
 **/
@@ -36,22 +36,41 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <errno.h>
+#include <time.h>
+
+int printime()
+{
+  time_t now;
+  char tstr[25];
+  struct tm* tm_info;
+  int utime=0;
+
+  utime=time(&now);
+  tm_info=localtime(&now);
+  strftime(tstr,25,"%Y-%m-%d %H:%M:%S ",tm_info);
+  printf("%s",tstr);
+
+  return utime;
+}
 
 void printusage()
 {
-  printf("usage: pipictest -a address [-n N] [-i] [-h] [-v] [-V]\n");
+  printf("usage: pipictest -a address [-n N] [-i] [-c] [-0] [-1] [-3] [-h] [-v] [-V]\n");
 }
 
 void printversion()
 {
-  printf("pipictest v. 20130814, Jaakko Koivuniemi\n");
+  printf("pipictest v. 20130819, Jaakko Koivuniemi\n");
 }
-
 
 int main(int argc, char **argv)
 {  
   int verb=0; // 1=verbosed output
   int testi2c=0; // test i2c data flow 
+  int ccycle=0; // count clock cycles
+  int analog0=0; // read in AN0 analog voltage
+  int analog1=0; // read in AN1 analog voltage
+  int analog3=0; // read in AN3 analog voltage
   int fd;
   char fileName[100]="/dev/i2c-1";
   int  address=0x00;
@@ -60,11 +79,15 @@ int main(int argc, char **argv)
   int i=0,j=0;
   int errcnt=0; // error count
   int nrpt=1; // number of times to repeate test function
+  time_t now;
+  int rtime=0; // cycle time from PIC
+  int t1=0,t2=0,dt=0; // start and end time of cycle count
+  int ain0=0,ain1=0,ain3=0; // analog input voltage
 
   int optch=0;
   while(optch!=-1)
     {
-      optch=getopt(argc,argv,"a:n:ihvV");
+      optch=getopt(argc,argv,"a:n:i013chvV");
       if(optch=='a')
 	{
 	  sscanf(optarg,"%X",&address);
@@ -76,6 +99,22 @@ int main(int argc, char **argv)
       if(optch=='i')
 	{
           testi2c=1;          
+	}
+      if(optch=='c')
+	{
+          ccycle=1;          
+	}
+      if(optch=='0')
+	{
+          analog0=1;          
+	}
+      if(optch=='1')
+	{
+          analog1=1;          
+	}
+      if(optch=='3')
+	{
+          analog3=1;          
 	}
       if(optch=='v')
 	{
@@ -114,21 +153,33 @@ int main(int argc, char **argv)
      return -1;
   }
 
-  if(testi2c==1)
+  if((verb==1)&&(testi2c==1)) printf("                    send         received\n"); 
+  if(ccycle==1)
   {
-     srand((unsigned int)time(NULL));
-
-     errcnt=0;
-     if(verb==1) printf("send         received\n");
-     for(j=0;j<nrpt;j++)
+     printf("reset timer\n");
+     t1=printime();
+     buf[0]=0x50; // reset PIC timer
+     if((write(fd, buf, 1)) != 1) 
      {
+          perror("Error writing to i2c slave");
+          return -1;
+     }
+     printf("cycles\n");
+  } 
+
+  srand((unsigned int)time(NULL));
+  for(j=0;j<nrpt;j++)
+  {
+     if(testi2c==1)
+     {
+        if(verb==1) printime();
+        errcnt=0;
         for(i=0;i<4;i++)
         {
            buf[i+1]=rand();
            send[i]=buf[i+1];
         }
         buf[0]=2;
-
         if(verb==1) 
         {
            printf("0x%02x%02x%02x%02x",buf[1],buf[2],buf[3],buf[4]);
@@ -149,6 +200,7 @@ int main(int argc, char **argv)
            if(verb==1)printf("0x%02x%02x%02x%02x\n",buf[0],buf[1],buf[2],buf[3]); 
            if((send[0]!=buf[0])||(send[1]!=buf[1])||(send[2]!=buf[2])||(send[3]!=buf[3])) 
            {
+              printime();
               printf("0x%02x%02x%02x%02x",send[0],send[1],send[2],send[3]);
               printf(" !=");
               printf("0x%02x%02x%02x%02x\n",buf[0],buf[1],buf[2],buf[3]); 
@@ -156,8 +208,106 @@ int main(int argc, char **argv)
            }
         }
      }
-     printf("Errors %d\n",errcnt);
+
+     if(ccycle==1)
+     {
+        if(verb==1) t2=printime();
+        buf[0]=0x51;
+        if((write(fd, buf, 1)) != 1) 
+        {
+           perror("Error writing to i2c slave");
+           return -1;
+        }
+        if(read(fd, buf,4)!=4) 
+        {
+           perror("Unable to read from slave");
+           return -1;
+        }
+        else 
+        {
+           if(verb==1)printf("0x%02x%02x%02x%02x ",buf[0],buf[1],buf[2],buf[3]); 
+           rtime=16777216*buf[0]+65536*buf[1]+256*buf[2]+buf[3];
+           if(verb==1) printf ("%d\n",rtime);
+        }
+     }
+
+     if((analog0==1)||(analog1==1)||(analog3==1)) t2=printime();
+     if(analog0==1)
+     {
+        buf[0]=0x40;
+        if((write(fd, buf, 1)) != 1) 
+        {
+           perror("Error writing to i2c slave");
+           return -1;
+        }
+        if(read(fd, buf,2)!=2) 
+        {
+           perror("Unable to read from slave");
+           return -1;
+        }
+        else 
+        {
+           if(verb==1)printf("0x%02x%02x ",buf[0],buf[1]);
+           ain0=256*buf[0]+buf[1];
+           printf ("%d ",ain0);
+        }
+     }
+
+     if(analog1==1)
+     {
+        buf[0]=0x41;
+        if((write(fd, buf, 1)) != 1) 
+        {
+           perror("Error writing to i2c slave");
+           return -1;
+        }
+        if(read(fd, buf,2)!=2) 
+        {
+           perror("Unable to read from slave");
+           return -1;
+        }
+        else 
+        {
+           if(verb==1)printf("0x%02x%02x ",buf[0],buf[1]);
+           ain1=256*buf[0]+buf[1];
+           printf ("%d ",ain1);
+        }
+     }
+
+     if(analog3==1)
+     {
+        buf[0]=0x43;
+        if((write(fd, buf, 1)) != 1) 
+        {
+           perror("Error writing to i2c slave");
+           return -1;
+        }
+        if(read(fd, buf,2)!=2) 
+        {
+           perror("Unable to read from slave");
+           return -1;
+        }
+        else 
+        {
+           if(verb==1)printf("0x%02x%02x ",buf[0],buf[1]);
+           ain3=256*buf[0]+buf[1];
+           printf ("%d ",ain3);
+        }
+     }
+
+     if((analog0==1)||(analog1==1)||(analog3==1)) printf("\n");
+
   }
+
+  if(ccycle==1)
+  {
+    t2=printime();
+    dt=t2-t1;
+    printf("elapsed seconds %d for %d cycles\n",dt,rtime);
+    if(dt>0) printf("%f s/cycle\n",((float)dt)/((float)rtime));
+  }
+  
+  if(testi2c==1) printf("i2c errors %d\n",errcnt);
 
   return 0;
 }
