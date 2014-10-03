fecf3bbf738f29cd6c9337b8c6561f18d073edfb
Jaakko Koivuniemi <oh7bf@sral.fi>
2014-10-03 20:27:01 +0200

    pipichbd.c add forgotten header file

---
+++ src/pipicswd.h
@@ -0,0 +1,8 @@
+#ifndef PIPICSWD_H_INCLUDED
+#define PIPICSWD_H_INCLUDED
+extern const char *i2cdev;// i2c device
+extern const int address; // i2c address
+extern const int i2lockmax; // maximum number of times to try lock i2c port  
+extern int loglev; // log level
+extern const char *logfile; // log file
+#endif
