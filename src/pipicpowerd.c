508063fcb92a4f9468f1899b350c24f7cdfdca7a
Jaakko Koivuniemi <oh7bf@sral.fi>
2014-10-05 12:58:52 +0200

    pipicpowerd.c separate common routines
    The functions logmessage.c, writecmd.c, readdata.c and testi2c.c are now in
    separate files. The same routines are used by pipicswd.c and pipichbd.c.

--- src/pipicpowerd.c
+++ src/pipicpowerd.c
@@ -21,7 +21,7 @@
  ****************************************************************************
  *
  * Mon Sep 30 18:51:20 CEST 2013
- * Edit: Mon Jul 14 20:24:37 CEST 2014
+ * Edit: Sun Oct  5 12:38:55 CEST 2014
  *
  * Jaakko Koivuniemi
  **/
@@ -40,8 +40,13 @@
 #include <time.h>
 #include <signal.h>
 #include <syslog.h>
+#include "pipicpowerd.h"
+#include "logmessage.h"
+#include "writecmd.h"
+#include "readdata.h"
+#include "testi2c.h"
 
-const int version=20140714; // program version
+const int version=20141005; // program version
 
 int voltint=300; // battery voltage reading interval [s]
 int buttonint=10; // button reading interval [s]
@@ -69,7 +74,7 @@ int forcereset=0; // force PIC timer reset if i2c test fails
 int forceoff=0; // force power off after give PIC counter cycles
 int forceon=0; // force power up after give PIC counter cycles
 
-const char i2cdev[100]="/dev/i2c-1";
+const char *i2cdev="/dev/i2c-1";
 const int  address=0x26;
 const int  i2lockmax=10; // maximum number of times to try lock i2c port  
 
@@ -94,7 +99,7 @@ const char wifistate[200]="/sys/class/net/wlan0/operstate";
 const char pidfile[200]="/var/run/pipicpowerd.pid";
 
 int loglev=3;
-const char logfile[200]="/var/log/pipicpowerd.log";
+const char *logfile="/var/log/pipicpowerd.log";
 char message[200]="";
 int logstats=0;
 const char statfile[200]="/var/log/pipicpowers.log";
@@ -105,34 +110,6 @@ const char atpwrdown[200]="/usr/sbin/atpwrdown";
 
 int pwroff=0; // 1==SIGTERM causes power off, 2==no power up in future, 3==power cycle  
 int settime=1; // 1==set system time from PIC counter
-
-void logmessage(const char logfile[200], const char message[200], int loglev, int msglev)
-{
-  time_t now;
-  char tstr[25];
-  struct tm* tm_info;
-  FILE *log;
-
-  time(&now);
-  tm_info=localtime(&now);
-  strftime(tstr,25,"%Y-%m-%d %H:%M:%S",tm_info);
-
-  if(msglev>=loglev)
-  {
-    log=fopen(logfile, "a");
-    if(NULL==log)
-    {
-      perror("could not open log file");
-    }
-    else
-    { 
-      fprintf(log,"%s ",tstr);
-      fprintf(log,"%s\n",message);
-      fclose(log);
-    }
-
-  }
-}
 
 // write usage statistics
 void writestat(const char statfile[200], unsigned unxstart, unsigned unxstop, int timerstart, int timerstop, float Vmin, float Vave, float Vmax, float Tmin, float Tave, float Tmax, float Tcpumin, float Tcpuave, float Tcpumax, int wifiuptime)
@@ -408,246 +385,6 @@ int read_wakeup()
   }
 
   return wtime;
-}
-
-
-// write i2c command to PIC optionally followed by data, length is the number 
-// of bytes and can be 0, 1, 2 or 4 
-// return: 1=ok, -1=open failed, -2=lock failed, -3=bus access failed, 
-// -4=i2c slave writing failed
-int write_cmd(int cmd, int data, int length)
-{
-  int ok=0;
-  int fd,rd;
-  int rnxt=0;
-  int cnt=0;
-  unsigned char buf[10];
-
-  if((cmd>=0)&&(cmd<=255))
-  {
-    if((fd=open(i2cdev, O_RDWR)) < 0) 
-    {
-      sprintf(message,"Failed to open i2c port");
-      logmessage(logfile,message,loglev,4);
-      return -1;
-    }
-
-    rd=flock(fd, LOCK_EX|LOCK_NB);
-
-    cnt=i2lockmax;
-    while((rd==1)&&(cnt>0)) // try again if port locking failed
-    {
-      sleep(1);
-      rd=flock(fd, LOCK_EX|LOCK_NB);
-      cnt--;
-    }
-
-    if(rd)
-    {
-      sprintf(message,"Failed to lock i2c port");
-      logmessage(logfile,message,loglev,4);
-      return -2;
-    }
-
-    if(ioctl(fd, I2C_SLAVE, address) < 0) 
-    {
-      sprintf(message,"Unable to get bus access to talk to slave");
-      logmessage(logfile,message,loglev,4);
-      return -3;
-    }
-
-    buf[0]=cmd;
-    if(length==1)
-    { 
-      buf[1]=data;
-      sprintf(message,"Send 0x%02x%02x",buf[0],buf[1]);
-      logmessage(logfile,message,loglev,1);
-      if((write(fd, buf, 2)) != 2) 
-      {
-        sprintf(message,"Error writing to i2c slave");
-        logmessage(logfile,message,loglev,4);
-        return -4;
-      }
-   }
-   else if(length==2)
-   { 
-      buf[1]=(int)(data/256);
-      buf[2]=data%256;
-      sprintf(message,"Send 0x%02x%02x%02x",buf[0],buf[1],buf[2]);
-      logmessage(logfile,message,loglev,1); 
-      if((write(fd, buf, 3)) != 3) 
-      {
-        sprintf(message,"Error writing to i2c slave");
-        logmessage(logfile,message,loglev,4);
-        return -4;
-      }
-   }
-   else if(length==4)
-   { 
-      buf[1]=(int)(data/16777216);
-      rnxt=data%16777216;
-      buf[2]=(int)(rnxt/65536);
-      rnxt=rnxt%65536;
-      buf[3]=(int)(rnxt/256);
-      buf[4]=rnxt%256;
-      sprintf(message,"Send 0x%02x%02x%02x%02x%02x",buf[0],buf[1],buf[2],buf[3],buf[4]);
-      logmessage(logfile,message,loglev,1); 
-      if((write(fd, buf, 5)) != 5) 
-      {
-        sprintf(message,"Error writing to i2c slave");
-        logmessage(logfile,message,loglev,4);
-        return -4;
-      }
-   }
-   else
-   {
-      sprintf(message,"Send 0x%02x",buf[0]);
-      logmessage(logfile,message,loglev,1); 
-      if((write(fd, buf, 1)) != 1) 
-      {
-        sprintf(message,"Error writing to i2c slave");
-        logmessage(logfile,message,loglev,4);
-        return -4;
-      }
-   }
-   close(fd);
-   ok=1;
-  }
-  
-  return ok;
-}
-
-// read data with i2c from PIC, length is the number of bytes to read 
-// return: -1=open failed, -2=lock failed, -3=bus access failed, 
-// -4=i2c slave reading failed
-int read_data(int length)
-{
-  int rdata=0;
-  int fd,rd;
-  int cnt=0;
-  unsigned char buf[10];
-
-  if((fd=open(i2cdev, O_RDWR)) < 0) 
-  {
-    sprintf(message,"Failed to open i2c port");
-    logmessage(logfile,message,loglev,4);
-    return -1;
-  }
-
-  rd=flock(fd, LOCK_EX|LOCK_NB);
-
-  cnt=i2lockmax;
-  while((rd==1)&&(cnt>0)) // try again if port locking failed
-  {
-    sleep(1);
-    rd=flock(fd, LOCK_EX|LOCK_NB);
-    cnt--;
-  }
-
-  if(rd)
-  {
-    sprintf(message,"Failed to lock i2c port");
-    logmessage(logfile,message,loglev,4);
-    return -2;
-  }
-
-  if(ioctl(fd, I2C_SLAVE, address) < 0) 
-  {
-    sprintf(message,"Unable to get bus access to talk to slave");
-    logmessage(logfile,message,loglev,4);
-    return -3;
-  }
-
-  if(length==1)
-  {
-     if(read(fd, buf,1)!=1) 
-     {
-       sprintf(message,"Unable to read from slave");
-       logmessage(logfile,message,loglev,4);
-       return -4;
-     }
-     else 
-     {
-       sprintf(message,"Receive 0x%02x",buf[0]);
-       logmessage(logfile,message,loglev,1); 
-       rdata=buf[0];
-     }
-  } 
-  else if(length==2)
-  {
-     if(read(fd, buf,2)!=2) 
-     {
-       sprintf(message,"Unable to read from slave");
-       logmessage(logfile,message,loglev,4);
-       return -4;
-     }
-     else 
-     {
-       sprintf(message,"Receive 0x%02x%02x",buf[0],buf[1]);
-       logmessage(logfile,message,loglev,1);  
-       rdata=256*buf[0]+buf[1];
-     }
-  }
-  else if(length==4)
-  {
-     if(read(fd, buf,4)!=4) 
-     {
-       sprintf(message,"Unable to read from slave");
-       logmessage(logfile,message,loglev,4);
-       return -4;
-     }
-     else 
-     {
-        sprintf(message,"Receive 0x%02x%02x%02x%02x",buf[0],buf[1],buf[2],buf[3]);
-        logmessage(logfile,message,loglev,1);  
-        rdata=16777216*buf[0]+65536*buf[1]+256*buf[2]+buf[3];
-     }
-  }
-
-  close(fd);
-
-  return rdata;
-}
-
-
-// send 4 test bytes to PiPIC and read them back 
-int testi2c()
-{
-  int ok=-1;
-  int testint=0;
-  int testres=1;
-
-  srand((unsigned int)time(NULL));
-
-  testint=rand();
-  while((testint==-1)||(testint==-2)||(testint==-3)||(testint==-4))
-  {
-    testint=rand();
-  }
-
-  ok=write_cmd(0x02,testint,4);
-
-  if(ok!=1)
-  {
-    strcpy(message,"failed to write 4 test bytes"); 
-    logmessage(logfile,message,loglev,4);
-  }
-  else
-  {
-    testres=read_data(4);
-
-    if((testres==-1)||(testres==-2)||(testres==-3)||(testres==-4))
-    {
-      strcpy(message,"failed to read 4 test bytes"); 
-      logmessage(logfile,message,loglev,4);
-    }
-    else
-    {
-      if(testint==testres) ok=1; else ok=0;
-    }
-  }
-
-  return ok;
 }
 
 // read voltage from PIC AN3
---
+++ src/pipicpowerd.h
@@ -0,0 +1,8 @@
+#ifndef PIPICPOWERD_H_INCLUDED
+#define PIPICPOWERD_H_INCLUDED
+extern const char *i2cdev;// i2c device
+extern const int address; // i2c address
+extern const int i2lockmax; // maximum number of times to try lock i2c port  
+extern int loglev; // log level
+extern const char *logfile; // log file
+#endif
