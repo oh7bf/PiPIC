ed98658bf5094b0a25b8711559959d8d194ce02c
Jaakko Koivuniemi <oh7bf@sral.fi>
2020-12-29 19:53:09 -0600

    readdata.c and writecmd.c bug fix in /dev/i2c-1 locking
    From mladenva "flock can never return 1, it returns 0 on success and -1
    in case of problem, so this while loops ends after 1st failed flock,..."
    
    Thanks for pointing this out and sorry for delay in fixing it.

--- src/readdata.c
+++ src/readdata.c
@@ -13,88 +13,88 @@
 // -4=i2c slave reading failed
 int read_data(int length)
 {
-  int rdata=0;
-  int fd,rd;
-  int cnt=0;
-  unsigned char buf[10];
-  char message[200]="";
+  int rdata = 0;
+  int fd, rd;
+  int cnt = 0;
+  unsigned char buf[ 10 ];
+  char message[ 200 ] = "";
 
-  if((fd=open(i2cdev, O_RDWR)) < 0) 
+  if( (fd = open( i2cdev, O_RDWR) ) < 0) 
   {
-    syslog(LOG_ERR, "Failed to open i2c port");
+    syslog( LOG_ERR, "Failed to open i2c port");
     return -1;
   }
 
-  rd=flock(fd, LOCK_EX|LOCK_NB);
+  rd = flock(fd, LOCK_EX | LOCK_NB);
 
-  cnt=i2lockmax;
-  while((rd==1)&&(cnt>0)) // try again if port locking failed
+  cnt = i2lockmax;
+  while( rd != 0 && cnt > 0 ) // try again if port locking failed
   {
-    sleep(1);
-    rd=flock(fd, LOCK_EX|LOCK_NB);
+    sleep( 1 );
+    rd = flock( fd, LOCK_EX | LOCK_NB);
     cnt--;
   }
-  if(rd)
+  if( rd )
   {
-    syslog(LOG_ERR, "Failed to lock i2c port");
-    close(fd);
+    syslog( LOG_ERR, "Failed to lock i2c port");
+    close( fd );
     return -2;
   }
 
-  if(ioctl(fd, I2C_SLAVE, address) < 0) 
+  if( ioctl( fd, I2C_SLAVE, address) < 0 ) 
   {
-    syslog(LOG_ERR, "Unable to get bus access to talk to slave");
-    close(fd);
+    syslog( LOG_ERR, "Unable to get bus access to talk to slave");
+    close( fd );
     return -3;
   }
 
-  if(length==1)
+  if( length == 1 )
   {
-     if(read(fd, buf,1)!=1) 
+     if( read( fd, buf, 1 ) != 1 ) 
      {
-       syslog(LOG_ERR, "Unable to read from slave");
-       close(fd);
+       syslog( LOG_ERR, "Unable to read from slave");
+       close( fd );
        return -4;
      }
      else 
      {
-       sprintf(message, "Receive 0x%02x",buf[0]);
-       syslog(LOG_DEBUG, "%s", message); 
-       rdata=buf[0];
+       sprintf( message, "Receive 0x%02x", buf[ 0 ] );
+       syslog( LOG_DEBUG, "%s", message); 
+       rdata = buf[ 0 ];
      }
   } 
-  else if(length==2)
+  else if( length == 2 )
   {
-     if(read(fd, buf,2)!=2) 
+     if( read( fd, buf, 2 ) != 2 ) 
      {
-       syslog(LOG_ERR, "Unable to read from slave");
-       close(fd);
+       syslog( LOG_ERR, "Unable to read from slave");
+       close( fd );
        return -4;
      }
      else 
      {
-       sprintf(message,"Receive 0x%02x%02x",buf[0],buf[1]);
-       syslog(LOG_DEBUG, "%s", message);  
-       rdata=256*buf[0]+buf[1];
+       sprintf( message, "Receive 0x%02x%02x", buf[0], buf[1] );
+       syslog( LOG_DEBUG, "%s", message);  
+       rdata = 256 * buf[0] + buf[1];
      }
   }
-  else if(length==4)
+  else if( length == 4 )
   {
-     if(read(fd, buf,4)!=4) 
+     if( read( fd, buf, 4) != 4 ) 
      {
-       syslog(LOG_ERR, "Unable to read from slave");
-       close(fd);
+       syslog( LOG_ERR, "Unable to read from slave");
+       close( fd );
        return -4;
      }
      else 
      {
-        sprintf(message,"Receive 0x%02x%02x%02x%02x",buf[0],buf[1],buf[2],buf[3]);
-        syslog(LOG_DEBUG, "%s", message);  
-        rdata=16777216*buf[0]+65536*buf[1]+256*buf[2]+buf[3];
+        sprintf( message, "Receive 0x%02x%02x%02x%02x", buf[0], buf[1], buf[2], buf[3] );
+        syslog( LOG_DEBUG, "%s", message);  
+        rdata = 16777216 * buf[ 0 ] + 65536 * buf[ 1 ] + 256 * buf[ 2 ] + buf[ 3 ];
      }
   }
 
-  close(fd);
+  close( fd );
 
   return rdata;
 }
--- src/writecmd.c
+++ src/writecmd.c
@@ -14,100 +14,101 @@
 // -4=i2c slave writing failed
 int write_cmd(int cmd, int data, int length)
 {
-  int ok=0;
-  int fd,rd;
-  int rnxt=0;
-  int cnt=0;
-  unsigned char buf[10];
-  char message[200]="";
+  int ok = 0;
+  int fd, rd;
+  int rnxt = 0;
+  int cnt = 0;
+  unsigned char buf[ 10 ];
+  char message[ 200 ] = "";
 
-  if((cmd>=0)&&(cmd<=255))
+  if( cmd >= 0 && cmd <= 255 )
   {
-    if((fd=open(i2cdev, O_RDWR)) < 0)
+    if( ( fd = open( i2cdev, O_RDWR) ) < 0 )
     {
-      syslog(LOG_ERR, "Failed to open i2c port");
+      syslog( LOG_ERR, "Failed to open i2c port");
       return -1;
     }
 
-    rd=flock(fd, LOCK_EX|LOCK_NB);
+    rd = flock( fd, LOCK_EX | LOCK_NB);
 
-    cnt=i2lockmax;
-    while((rd==1)&&(cnt>0)) // try again if port locking failed
+    cnt = i2lockmax;
+    while( rd != 0 && cnt > 0 ) // try again if port locking failed
     {
-      sleep(1);
-      rd=flock(fd, LOCK_EX|LOCK_NB);
+      sleep( 1 );
+      rd = flock( fd, LOCK_EX | LOCK_NB);
       cnt--;
     }
 
-    if(rd)
+    if( rd )
     {
-      syslog(LOG_ERR, "Failed to lock i2c port");
-      close(fd);
+      syslog( LOG_ERR, "Failed to lock i2c port");
+      close( fd );
       return -2;
     }
 
-    if(ioctl(fd, I2C_SLAVE, address) < 0)
+    if( ioctl( fd, I2C_SLAVE, address) < 0 )
     {
-      syslog(LOG_ERR, "Unable to get bus access to talk to slave");
-      close(fd);
+      syslog( LOG_ERR, "Unable to get bus access to talk to slave");
+      close( fd );
       return -3;
     }
-    buf[0]=cmd;
-    if(length==1)
+
+    buf[ 0 ] = cmd;
+    if( length == 1 )
     {
-      buf[1]=data;
-      sprintf(message,"Send 0x%02x%02x",buf[0],buf[1]);
-      syslog(LOG_DEBUG, "%s", message);
-      if((write(fd, buf, 2)) != 2)
+      buf[ 1 ] = data;
+      sprintf( message, "Send 0x%02x%02x", buf[ 0 ], buf[ 1 ] );
+      syslog( LOG_DEBUG, "%s", message);
+      if( write(fd, buf, 2)  != 2 )
       {
-        syslog(LOG_ERR, "Error writing to i2c slave");
-        close(fd);
+        syslog( LOG_ERR, "Error writing to i2c slave");
+        close( fd );
         return -4;
       }
    }
-   else if(length==2)
+   else if( length == 2 )
    {
-      buf[1]=(int)(data/256);
-      buf[2]=data%256;
-      sprintf(message,"Send 0x%02x%02x%02x",buf[0],buf[1],buf[2]);
-      syslog(LOG_DEBUG, "%s", message);
-      if((write(fd, buf, 3)) != 3)
+      buf[ 1 ]=(int)( data / 256 );
+      buf[ 2 ] = data % 256;
+      sprintf( message, "Send 0x%02x%02x%02x", buf[ 0 ], buf[ 1 ], buf[ 2 ] );
+      syslog( LOG_DEBUG, "%s", message);
+      if( write( fd, buf, 3)  != 3 )
       {
-        syslog(LOG_ERR, "Error writing to i2c slave");
-        close(fd);
+        syslog( LOG_ERR, "Error writing to i2c slave");
+        close( fd );
         return -4;
       }
    }
-   else if(length==4)
+   else if( length == 4 )
    {
-      buf[1]=(int)(data/16777216);
-      rnxt=data%16777216;
-      buf[2]=(int)(rnxt/65536);
-      rnxt=rnxt%65536;
-      buf[3]=(int)(rnxt/256);
-      buf[4]=rnxt%256;
-      sprintf(message,"Send 0x%02x%02x%02x%02x%02x",buf[0],buf[1],buf[2],buf[3],buf[4]);
-      syslog(LOG_DEBUG, "%s", message);
-      if((write(fd, buf, 5)) != 5)
+      buf[ 1 ] = (int)( data / 16777216 );
+      rnxt = data % 16777216;
+      buf[ 2 ] = (int)( rnxt / 65536 );
+      rnxt = rnxt % 65536;
+      buf[ 3 ] = (int)( rnxt / 256 );
+      buf[ 4 ] = rnxt % 256;
+      sprintf( message, "Send 0x%02x%02x%02x%02x%02x", buf[ 0 ], buf[ 1 ], buf[ 2 ], buf[ 3 ], buf[ 4 ] );
+      syslog( LOG_DEBUG, "%s", message);
+      if( write(fd, buf, 5) != 5 )
       {
-        syslog(LOG_ERR, "Error writing to i2c slave");
-        close(fd);
+        syslog( LOG_ERR, "Error writing to i2c slave" );
+        close( fd );
         return -4;
       }
    }
    else
    {
-      sprintf(message,"Send 0x%02x",buf[0]);
-      syslog(LOG_DEBUG, "%s", message);
-      if((write(fd, buf, 1)) != 1)
+      sprintf( message, "Send 0x%02x", buf[ 0 ] );
+      syslog( LOG_DEBUG, "%s", message );
+      if( write(fd, buf, 1) != 1 )
       {
-        syslog(LOG_ERR, "Error writing to i2c slave");
-        close(fd);
+        syslog( LOG_ERR, "Error writing to i2c slave" );
+        close( fd );
         return -4;
       }
    }
-   close(fd);
-   ok=1;
+   close( fd );
+   ok = 1;
   }
 
   return ok;
